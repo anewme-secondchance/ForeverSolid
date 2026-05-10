dde50183b4d4df3c8d4772ee0a0a4b3335396eb1
TaDasha D. Jones <jonestadasha@gmail.com>
2026-05-10 11:23:43 -0500

    Initialize App component with basic layout

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="home">
+
+      <h1 className="title">FOREVER SOLID</h1>
+
+      <p className="subtitle">
+        It’s more than clothing, it’s a mindset.
+      </p>
+
+      <div className="logo">
+        Forever Solid
+      </div>
+
+      <p className="quote">
+        “You can’t change how solid you are
+        but you can change who you’re solid to”
+      </p>
+
+      <button className="btn">
+        Shop Now
+      </button>
+
+    </div>
+  );
+}
+
+export default App;
