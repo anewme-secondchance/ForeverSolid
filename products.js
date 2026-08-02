b7a434fcb9086df184c2e26b5b6a44067017e707
TaDasha D. Jones <jonestadasha@gmail.com>
2026-08-01 20:37:11 -0500

    Add Forever Solid and Green Box Baby products

---
+++ products.js
@@ -0,0 +1,511 @@
+/*=========================================
+        FOREVER SOLID PRODUCTS
+=========================================*/
+
+const products = [
+
+/*==============================
+        FOREVER SOLID
+==============================*/
+
+{
+    id:"fs1",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS1.jpeg",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs2",
+    brand:"Forever Solid",
+    name:"Forever Solid Signature Tee",
+    category:"Shirts",
+    image:"images/IMG_FS2.jpeg",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs3",
+    brand:"Forever Solid",
+    name:"Forever Solid Graphic Tee",
+    category:"Shirts",
+    image:"images/IMG_FS3.PNG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs4",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS4.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs5",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS5.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs6",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS6.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs7",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS7.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs8",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS8.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs9",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS9.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs10",
+    brand:"Forever Solid",
+    name:"Forever Solid Tee",
+    category:"Shirts",
+    image:"images/IMG_FS10.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs11",
+    brand:"Forever Solid",
+    name:"Never Did Tee",
+    category:"Shirts",
+    image:"images/IMG_FS11.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs12",
+    brand:"Forever Solid",
+    name:"Pink Signature Tee",
+    category:"Shirts",
+    image:"images/IMG_FS12.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs13",
+    brand:"Forever Solid",
+    name:"Family Matching Set",
+    category:"Family",
+    image:"images/IMG_FS13.JPEG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs14",
+    brand:"Forever Solid",
+    name:"Lil Cacc Lil Hawk Tee",
+    category:"Shirts",
+    image:"images/IMG_FS14.jpeg",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs15",
+    brand:"Forever Solid",
+    name:"Gray Shorts Set",
+    category:"Short Sets",
+    image:"images/IMG_FS15.jpeg",
+    priceSmall:55,
+    priceLarge:65
+}
+
+];
+
+/*==============================
+        FOREVER SOLID
+==============================*/
+
+,
+{
+    id:"fs16",
+    brand:"Forever Solid",
+    name:"Navy Shorts Set",
+    category:"Short Sets",
+    image:"images/IMG_FS16.jpeg",
+    priceSmall:55,
+    priceLarge:65
+},
+
+{
+    id:"fs17",
+    brand:"Forever Solid",
+    name:"Navy Shorts Set Back",
+    category:"Short Sets",
+    image:"images/IMG_FS17.JPG",
+    priceSmall:55,
+    priceLarge:65
+},
+
+{
+    id:"fs18",
+    brand:"Forever Solid",
+    name:"Family Matching Black Set",
+    category:"Family",
+    image:"images/IMG_FS18.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs19",
+    brand:"Forever Solid",
+    name:"White & Green Long Sleeve",
+    category:"Long Sleeve",
+    image:"images/IMG_FS19.JPG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs20",
+    brand:"Forever Solid",
+    name:"Red Hoodie",
+    category:"Hoodies",
+    image:"images/IMG_FS20.JPG",
+    priceSmall:55,
+    priceLarge:60
+},
+
+{
+    id:"fs21",
+    brand:"Forever Solid",
+    name:"Streetz Dead Long Sleeve",
+    category:"Long Sleeve",
+    image:"images/IMG_FS21.JPEG",
+    priceSmall:35,
+    priceLarge:40
+},
+
+{
+    id:"fs22",
+    brand:"Forever Solid",
+    name:"Red Shorts Set",
+    category:"Short Sets",
+    image:"images/IMG_FS22.jpeg",
+    priceSmall:70,
+    priceLarge:80
+},
+
+{
+    id:"fs23",
+    brand:"Forever Solid",
+    name:"Women's Black Shorts",
+    category:"Shorts",
+    image:"images/IMG_FS23.JPEG",
+    priceSmall:30,
+    priceLarge:40
+},
+
+{
+    id:"fs24",
+    brand:"Forever Solid",
+    name:"Women's Red Shorts",
+    category:"Shorts",
+    image:"images/IMG_FS24.JPEG",
+    priceSmall:30,
+    priceLarge:40
+},
+
+{
+    id:"fs25",
+    brand:"Forever Solid",
+    name:"Women's White Shorts",
+    category:"Shorts",
+    image:"images/IMG_FS25.JPEG",
+    priceSmall:30,
+    priceLarge:40
+},
+
+/*==============================
+            HATS
+==============================*/
+
+{
+    id:"hat1",
+    brand:"Forever Solid",
+    name:"Forever Solid Hat",
+    category:"Hats",
+    image:"images/IMG_FSHAT1.jpeg",
+    priceSmall:30,
+    priceLarge:30
+},
+
+{
+    id:"hat2",
+    brand:"Forever Solid",
+    name:"Forever Solid Snapback",
+    category:"Hats",
+    image:"images/IMG_FSHAT2.jpeg",
+    priceSmall:30,
+    priceLarge:30
+},
+
+/*==============================
+        GREEN BOX BABY
+==============================*/
+
+,
+{
+    id:"gb1",
+    brand:"Green Box Baby",
+    name:"Green Box Baby Graphic Shirt",
+    category:"Shirts",
+    image:"images/IMG_GREENBOX1.JPG",
+    priceSmall:35,
+    priceLarge:40,
+    description:"Premium Green Box Baby graphic shirt."
+},
+
+{
+    id:"gb2",
+    brand:"Green Box Baby",
+    name:"Green Box Baby Hoodie",
+    category:"Hoodies",
+    image:"images/IMG_GREENBOX2.JPG",
+    priceSmall:55,
+    priceLarge:60,
+    description:"Premium Green Box Baby hoodie."
+},
+
+{
+    id:"gb3",
+    brand:"Green Box Baby",
+    name:"Green Box Baby Hoodie & Shorts Set",
+    category:"Sets",
+    image:"images/IMG_GREENBOX3.jpeg",
+    priceSmall:70,
+    priceLarge:80,
+    description:"Matching hoodie and shorts set."
+},
+
+{
+    id:"gb4",
+    brand:"Green Box Baby",
+    name:"Green Box Baby T-Shirt",
+    category:"T-Shirts",
+    image:"images/IMG_GREENBOX4.jpeg",
+    priceSmall:25,
+    priceLarge:35,
+    description:"Classic Green Box Baby tee."
+},
+
+{
+    id:"gb5",
+    brand:"Green Box Baby",
+    name:"Green Box Baby Shorts",
+    category:"Shorts",
+    image:"images/IMG_GREENBOX5.jpeg",
+    priceSmall:30,
+    priceLarge:40,
+    description:"Premium Green Box Baby shorts."
+},
+
+{
+    id:"gb6",
+    brand:"Green Box Baby",
+    name:"Green Box Baby T-Shirt & Shorts Set",
+    category:"Sets",
+    image:"images/IMG_GREENBOX6.jpeg",
+    priceSmall:55,
+    priceLarge:65,
+    description:"Matching Green Box Baby t-shirt and shorts set."
+}
+
+  /*==============================
+        STORE SETTINGS
+==============================*/
+
+const STORE = {
+
+    name:"Forever Solid",
+
+    phone:"9032629694",
+
+    email:"",
+
+    city:"Tyler",
+
+    state:"Texas",
+
+    currency:"USD"
+
+};
+
+/*==============================
+        AVAILABLE SIZES
+==============================*/
+
+const SIZES = [
+
+"S",
+
+"M",
+
+"L",
+
+"XL",
+
+"2XL",
+
+"3XL",
+
+"4XL"
+
+];
+
+/*==============================
+        AVAILABLE COLORS
+==============================*/
+
+const COLORS = [
+
+"Black",
+
+"White",
+
+"Red",
+
+"Royal Blue",
+
+"Navy",
+
+"Gray",
+
+"Pink",
+
+"Green",
+
+"Purple",
+
+"Brown",
+
+"Tan",
+
+"Orange",
+
+"Yellow",
+
+"Other (Type Below)"
+
+];
+
+/*==============================
+        PRODUCT CATEGORIES
+==============================*/
+
+const CATEGORIES = [
+
+"Shirts",
+
+"T-Shirts",
+
+"Long Sleeve",
+
+"Hoodies",
+
+"Shorts",
+
+"Short Sets",
+
+"Sets",
+
+"Hats",
+
+"Family"
+
+];
+
+/*==============================
+        HELPER FUNCTIONS
+==============================*/
+
+function getProduct(id){
+
+    return products.find(product => product.id === id);
+
+}
+
+function getProductsByBrand(brand){
+
+    return products.filter(product => product.brand === brand);
+
+}
+
+function getProductsByCategory(category){
+
+    return products.filter(product => product.category === category);
+
+}
+
+function calculatePrice(product,size){
+
+    if(
+
+        size==="2XL" ||
+
+        size==="3XL" ||
+
+        size==="4XL"
+
+    ){
+
+        return product.priceLarge;
+
+    }
+
+    return product.priceSmall;
+
+}
+
+/*==============================
+        END PRODUCTS
+==============================*/
