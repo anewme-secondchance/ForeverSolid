0709f9e3ff258d7c08138ce0d46980c984f033b9
TaDasha D. Jones <jonestadasha@gmail.com>
2026-08-04 15:12:02 -0500

    Add trial license system constants in trail.js

---
+++ trail.js
@@ -0,0 +1,9 @@
+/*=========================================
+        TADIE4ENT LICENSE SYSTEM
+=========================================*/
+
+const APP_MODE = "TRIAL"; // TRIAL or FULL
+
+const TRIAL_DAYS = 7;
+
+const INSTALL_KEY = "fs_install_date";
